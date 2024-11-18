cea8229bf29001a7f628ae22cc3803e2b093f247
Amir.exe <112759041+Amirexe14@users.noreply.github.com>
2024-11-18 17:57:12 +0100

    Create Main

---
+++ Main.py
@@ -0,0 +1,30 @@
+import ctypes
+import time
+from PIL import Image
+
+gif_path = input("Enter File Path: (e.g: C:/Users/Amir/Downloads/Test.gif): ")
+temp_path = input("Enter Temp Path: (e.g: C:/Users/Amir/Desktop/Temp.bmp): ") #temp file to store gif frames
+
+def setBackground(path):
+    ctypes.windll.user32.SystemParametersInfoW(20, 0, path, 3)
+
+def processFrames(file_path):
+    with Image.open(file_path) as gif:
+        try:
+            while True:
+                gif.save(temp_path, format="BMP")
+
+                setBackground(temp_path)
+
+                frame_duration = gif.info.get("duration", 100) / 1000.0 #Get the duration in seconds 
+                time.sleep(frame_duration)
+
+                #Move to next frame:
+                gif.seek(gif.tell() + 1) #"This frame + 1"
+
+        except EOFError:
+            # End of frames
+            print("No other frame detected, restarting")
+            process_gif_frames(gif_path)
+
+process_gif_frames(gif_path)
